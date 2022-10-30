2ea9acd3cf2c8a8beb39a094a94a1c9dd7c7b0f8
jeanlandercy <jeanlandercy@live.com>
2022-10-30 22:21:28 +0100

    First step is reached, read/write function are there.

--- qrflow/qrflow/static/js/scanner.js
+++ qrflow/qrflow/static/js/scanner.js
@@ -18,11 +18,12 @@ docReady(function() {
             lastResult = decodedText;
             // Handle on success condition with the decoded message.
             console.log(`Scan result ${decodedText}`, decodedResult);
+            resultContainer.innerHTML = lastResult;
         }
     }
 
     var html5QrcodeScanner = new Html5QrcodeScanner(
-        "qr-reader", { fps: 10, qrbox: 250 }
+        "qr-reader", { fps: 5, qrbox: 250 }
     );
     html5QrcodeScanner.render(onScanSuccess);
 
