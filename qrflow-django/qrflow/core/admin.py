5735193519cbbc7baab9177e3bc778108668d9cf
jeanlandercy <jeanlandercy@live.com>
2022-11-12 15:52:01 +0100

    Baseline started

--- qrflow-django/qrflow/core/admin.py
+++ qrflow-django/qrflow/core/admin.py
@@ -7,3 +7,18 @@ from core import models
 @admin.register(models.CustomUser)
 class CustomUserAdmin(UserAdmin):
     pass
+
+
+@admin.register(models.Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+
+    def _users(self, obj):
+        print(dir(obj))
+        return ", ".join([item.username for item in obj.users.all()])
+
+    list_display = ('id', 'name', '_users')
+
+
+@admin.register(models.OrganizationMembership)
+class OrganizationMembershipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'organization')
--- qrflow-django/qrflow/core/models.py
+++ qrflow-django/qrflow/core/models.py
@@ -8,7 +8,7 @@ class BaseAbstractModel(models.Model):
 
     class Meta:
         abstract = True
-        ordering = ['-created_at']
+        ordering = ['-created']
 
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True, editable=False)
@@ -19,9 +19,28 @@ class BaseAbstractModel(models.Model):
         self.deleted = True
         self.save()
 
+    def __str__(self):
+        if hasattr(self, "name"):
+            return self.name
+        else:
+            return self.id
+
 
 class CustomUser(AbstractUser, BaseAbstractModel):
 
     def __str__(self):
         return self.username
 
+
+class Organization(BaseAbstractModel):
+    name = models.CharField(max_length=128, unique=True)
+    users = models.ManyToManyField("CustomUser", through="OrganizationMembership")
+
+
+class OrganizationMembership(BaseAbstractModel):
+
+    class Meta:
+        unique_together = (('user', 'organization'),)
+
+    user = models.ForeignKey(CustomUser, on_delete=models.RESTRICT)
+    organization = models.ForeignKey(Organization, on_delete=models.RESTRICT)
