fd30455eab90165f80c969c18747a69b181a1a3e
jeanlandercy <jeanlandercy@live.com>
2022-11-12 15:04:00 +0100

    Switching for Django but keeping FastAPI in the game

---
+++ qrflow-django/README.md
@@ -0,0 +1,2 @@
+# QRFlow + Django
+
---
+++ qrflow-django/qrflow/qrflow/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import redirect
+from django.conf import settings
+from django.views import View
+from django.views.static import serve
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class ProjectHomeView(TemplateView):
+
+    template_name = "qrflow/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    # def get(self, request, *args, **kwargs):
+    #     return redirect("index")
+
+
+class FileServerView(LoginRequiredMixin, View):
+
+    def get(self, request, path, *args, **kwargs):
+        return serve(request, path, document_root=settings.MEDIA_ROOT)
