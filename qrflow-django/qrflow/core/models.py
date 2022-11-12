60c1f6b1b282a56d008f15281d35c937de9cd53d
jeanlandercy <jeanlandercy@live.com>
2022-11-12 15:32:37 +0100

    Created Core User with UUID abtarct model

--- qrflow-django/qrflow/core/models.py
+++ qrflow-django/qrflow/core/models.py
@@ -1,8 +1,27 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractUser):
+class BaseAbstractModel(models.Model):
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True, editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def soft_delete(self):
+        self.deleted = True
+        self.save()
+
+
+class CustomUser(AbstractUser, BaseAbstractModel):
 
     def __str__(self):
         return self.username
+
--- qrflow-django/qrflow/qrflow/settings.py
+++ qrflow-django/qrflow/qrflow/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.UUIDAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = URL_PREFIX + 'media/'
 MEDIA_ROOT = os.getenv("APP_MEDIA_ROOT", BASE_DIR / 'media')
