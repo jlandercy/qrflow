8f2fbbea29501345eb04ca4741fbdda51f5e7a3b
jeanlandercy <jeanlandercy@live.com>
2022-11-12 18:02:55 +0100

    Cannot fake Uploaded file in save method (snifff)

--- qrflow-django/qrflow/pki/models.py
+++ qrflow-django/qrflow/pki/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from core.models import BaseAbstractModel, OwnershipAbstractModel
 from pki.toolbox import CertificateHelper
@@ -6,28 +7,25 @@ from pki.toolbox import CertificateHelper
 
 class Certificate(BaseAbstractModel, OwnershipAbstractModel):
 
-    def organization_public_directory(instance, filename):
+    def organization_public_path(instance, filename):
         return "organizations/{}/public/{}".format(instance.organization.id.hex, instance.id.hex + ".crt")
 
-    def organization_private_directory(instance, filename):
+    def organization_private_path(instance, filename):
         return "organizations/{}/private/{}".format(instance.organization.id.hex, instance.id.hex + ".key")
 
     name = models.CharField(max_length=128, unique=True)
-    public_key = models.FileField(upload_to=organization_public_directory, blank=True)
-    private_key = models.FileField(upload_to=organization_private_directory, blank=True)
+    public_key = models.FileField(upload_to=organization_public_path, blank=True)
+    private_key = models.FileField(upload_to=organization_private_path, blank=True)
 
     def save(
         self, force_insert=False, force_update=False, using=None, update_fields=None
     ):
 
-        print(self.private_key)
-        print(self.public_key)
-        print(type(self.private_key))
         if not(self.public_key and self.private_key):
             # Create new certificates:
             key, cert = CertificateHelper.create_ca()
             print(key, cert)
-            self.public_key = models.FileField(CertificateHelper.to_public_pem(cert))
-            self.private_key = models.FileField(CertificateHelper.to_private_pem(key))
-
-        super().save()
+            self.public_key = SimpleUploadedFile('x', CertificateHelper.to_public_pem(cert))
+            self.private_key = SimpleUploadedFile('y', CertificateHelper.to_private_pem(key))
+            print(self.public_key)
+            print(self.private_key)
