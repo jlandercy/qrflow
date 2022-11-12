a40f1569beab840864f25f6784f881e0824f1452
jeanlandercy <jeanlandercy@live.com>
2022-11-12 16:51:26 +0100

    Fixed certificate file names

--- qrflow-django/qrflow/pki/models.py
+++ qrflow-django/qrflow/pki/models.py
@@ -5,12 +5,18 @@ from core.models import BaseAbstractModel, OwnershipAbstractModel
 
 class Certificate(BaseAbstractModel, OwnershipAbstractModel):
 
-    public_key = models.FileField(upload_to='organizations/org/public')
-    private_key = models.FileField(upload_to='organizations/org/private')
+    def organization_public_directory(instance, filename):
+        return "organizations/{}/public/{}".format(instance.organization.id.hex, instance.id.hex + ".crt")
+
+    def organization_private_directory(instance, filename):
+        return "organizations/{}/private/{}".format(instance.organization.id.hex, instance.id.hex + ".key")
+
+    public_key = models.FileField(upload_to=organization_public_directory)
+    private_key = models.FileField(upload_to=organization_private_directory)
 
     def save(
         self, force_insert=False, force_update=False, using=None, update_fields=None
     ):
         super().save()
         pass
-    
+
