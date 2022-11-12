bcce7dda62a4ae93caf3c2ce249bfa6b60293a3d
jeanlandercy <jeanlandercy@live.com>
2022-11-12 15:13:39 +0100

    Starting Django

--- qrflow-django/qrflow/qrflow/settings.py
+++ qrflow-django/qrflow/qrflow/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     "crispy_bootstrap5",
     # Project:
-    'guestmap',
+    'core',
 ]
 
 MIDDLEWARE = [
--- qrflow-django/qrflow/qrflow/urls.py
+++ qrflow-django/qrflow/qrflow/urls.py
@@ -1,19 +1,3 @@
-"""olimat URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include
@@ -26,7 +10,7 @@ urlpatterns = [
     path(r"media/<path:path>", views.FileServerView.as_view(), name="file-server"),
     path('admin/', admin.site.urls),
     path('account/', include(('django.contrib.auth.urls', 'django.contrib.auth'), namespace='account')),
-    path('guestmap/', include('guestmap.urls', namespace="guestmap")),
+    path('core/', include('core.urls', namespace="core")),
     path('', views.ProjectHomeView.as_view(), name='index'),
 ]
 
