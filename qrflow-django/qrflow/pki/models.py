a1f52a2ae83f907fcb3a2187f86f4a8feaea9def
jeanlandercy <jeanlandercy@live.com>
2022-11-12 16:35:21 +0100

    Added a bit of cryptography

--- qrflow-django/qrflow/pki/models.py
+++ qrflow-django/qrflow/pki/models.py
@@ -7,3 +7,10 @@ class Certificate(BaseAbstractModel, OwnershipAbstractModel):
 
     public_key = models.FileField(upload_to='organizations/org/public')
     private_key = models.FileField(upload_to='organizations/org/private')
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        super().save()
+        pass
+    
--- qrflow-django/qrflow/pki/toolbox.py
+++ qrflow-django/qrflow/pki/toolbox.py
@@ -17,12 +17,15 @@ class CertificateHelper:
         state_name="Brussels",
         locality_name="Brussels",
         organization_name="QR-Flow",
-        common_name="qrflow.com"
+        common_name="qrflow.com",
+        public_exponent=65537,
+        key_size=4096,
+        days=3650
     ):
 
         root_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=4096,
+            public_exponent=public_exponent,
+            key_size=key_size,
             backend=default_backend()
         )
         subject = issuer = x509.Name([
@@ -43,7 +46,7 @@ class CertificateHelper:
         ).not_valid_before(
             datetime.datetime.utcnow()
         ).not_valid_after(
-            datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+            datetime.datetime.utcnow() + datetime.timedelta(days=days)
         ).sign(root_key, hashes.SHA256(), default_backend())
 
         return root_key, root_cert
@@ -55,11 +58,14 @@ class CertificateHelper:
         state_name="Brussels",
         locality_name="Brussels",
         organization_name="QR-Flow",
-        dsn_name="qrflow.com"
+        dsn_name="qrflow.com",
+        public_exponent=65537,
+        key_size=4096,
+        days=365
     ):
         cert_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=4096,
+            public_exponent=public_exponent,
+            key_size=key_size,
             backend=default_backend()
         )
         new_subject = x509.Name([
@@ -79,7 +85,7 @@ class CertificateHelper:
         ).not_valid_before(
             datetime.datetime.utcnow()
         ).not_valid_after(
-            datetime.datetime.utcnow() + datetime.timedelta(days=365)
+            datetime.datetime.utcnow() + datetime.timedelta(days=days)
         ).add_extension(
             x509.SubjectAlternativeName([x509.DNSName(dsn_name)]),
             critical=False,
@@ -87,8 +93,16 @@ class CertificateHelper:
         return cert_key, cert
 
     @staticmethod
-    def to_pem(cert):
+    def to_public_pem(cert):
         return cert.public_bytes(serialization.Encoding.PEM)
+
+    @staticmethod
+    def to_private_pem(cert):
+        return cert.private_bytes(
+            serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        )
 
     @staticmethod
     def verify(cert, chain):
@@ -103,3 +117,14 @@ class CertificateHelper:
             return True
         except:
             return False
+
+
+if __name__ == "__main__":
+    root_key, root_cert = CertificateHelper.create_ca()
+
+    with open("../../../media/ca.crt", "wb") as handler:
+        handler.write(CertificateHelper.to_public_pem(root_cert))
+
+    with open("../../../media/ca.key", "wb") as handler:
+        handler.write(CertificateHelper.to_private_pem(root_key))
+
