c4f24f52eebfc81ebe091b59c3874ea1754a8dcd
jeanlandercy <jeanlandercy@live.com>
2022-11-26 16:25:42 +0100

    Binding Organization w/ Application in Generic views

--- qrflow-django/qrflow/core/urls.py
+++ qrflow-django/qrflow/core/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, re_path, include
 from django.conf.urls.static import static, serve
 
 
+from core import views
+
 app_name = 'core'
 
-urlpatterns = []
+urlpatterns = [
+    path(r"organization/", views.OrganizationListView.as_view(), name="organization-list"),
+    path(r"organization/<uuid:pk>/", views.OrganizationDetailView.as_view(), name="organization-detail"),
+]
--- qrflow-django/qrflow/core/views.py
+++ qrflow-django/qrflow/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from core import models
+from core.permissions import OrganizationPermissionMixin
+
+
+class OrganizationListView(OrganizationPermissionMixin, ListView):
+    model = models.Organization
+
+
+class OrganizationDetailView(OrganizationPermissionMixin, DetailView):
+    model = models.Organization
