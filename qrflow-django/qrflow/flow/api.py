8835e756f3a591f28331220d830717853ba863c7
jeanlandercy <jeanlandercy@live.com>
2022-11-27 11:01:21 +0100

    Adding Admin Filter

--- qrflow-django/qrflow/flow/api.py
+++ qrflow-django/qrflow/flow/api.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 
 #from myapps.serializers import UserSerializer
 
+from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import routers
@@ -30,5 +31,12 @@ class CodeViewSet(ApplicationPermissionMixin, viewsets.ModelViewSet):
     queryset = models.Code.objects.all()
 
 
+class QRCodeAPIView(generics.GenericAPIView):
+
+    def get(self, request, format=None):
+        return Response({})
+
+
 router.register('application', viewset=ApplicationViewSet)
 router.register('code', viewset=CodeViewSet)
+#router.register('code/generate', QRCodeAPIView.as_view(), basename='code_generate')
