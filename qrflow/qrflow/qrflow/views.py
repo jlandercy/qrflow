bbd51e581fb5921439b9c42f9f0d418b4f7ff4b4
jeanlandercy <jeanlandercy@live.com>
2023-10-14 10:17:12 +0200

    Limiting file server to organization

--- qrflow/qrflow/qrflow/views.py
+++ qrflow/qrflow/qrflow/views.py
@@ -4,6 +4,11 @@ from django.views import View
 from django.views.static import serve
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseNotFound
+
+from core.models import Organization
+from flow.models import Code
+from pki.models import Certificate
 
 
 class ProjectHomeView(TemplateView):
@@ -32,5 +37,17 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 
 class FileServerView(LoginRequiredMixin, View):
 
+    @staticmethod
+    def get_file_list(request):
+        organization = Organization.objects.get(id=request.COOKIES["organization"])
+        files = set()
+        files.update(Code.objects.filter(application__organization=organization).values_list("image", flat=True))
+        files.update(Certificate.objects.filter(organization=organization).values_list("public_key", flat=True))
+        return files
+
     def get(self, request, path, *args, **kwargs):
-        return serve(request, path, document_root=settings.MEDIA_ROOT)
+        files = self.get_file_list(request)
+        if path in files:
+            return serve(request, path, document_root=settings.MEDIA_ROOT)
+        else:
+            return HttpResponseNotFound("File not found")
